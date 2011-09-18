ed531ad34a34acad1c63064434fcfd18856455cb
stavros.baltas@gmail.com <stavros.baltas@gmail.com@abbbbd08-7383-e338-6a8c-29cca2b0d259>
2011-09-18 13:17:09 +0000

    Added new states
    Open base 
    New base 
    Destroy base
    power state 
    
    ---

---
+++ trunk/ project-singularity/Project Singularity GL/src/path2glory/SimpleGame/DestroyBase.java
@@ -0,0 +1,46 @@
+package path2glory.SimpleGame;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class DestroyBase extends BasicGameState{
+	
+    int stateID = 8;
+    
+
+    public DestroyBase(int stateID ) 
+    {
+       this.stateID = stateID;
+    }
+    
+	@Override
+	public int getID() {
+		  return stateID;
+	}
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
